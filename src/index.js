a938f58899edc35df40e7c490c03b40b39404069
github username <github email address>
2018-12-21 11:34:45 -0500

    sad face

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 1745 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +9,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  fetchImage(imageURL)
 })
+
+//set up function fetch to get image.
+
+function fetchImage(imageURL) {
+return fetch(imageURL).then(resp => resp.json())
+}
+//fetch just would not work for me :(
+
+
+function addImage(pic){
+  let imageArea = document.querySelector("#image")
+
+  let imageHTML = `
+  <img src="${pic.url}" id="image" data-id=""/>
+  `
+
+  imageArea.innerHTML += imageHTML
+}
+
+function addName(site) {
+  let nameArea = document.querySelector("#name")
+
+  let nameHTML = `
+  <img src="${site.name}" id="image" data-id=""/>
+  `
+
+  nameArea.innerHTML += nameHTML
+}
