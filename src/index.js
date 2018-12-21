6505c1e327147c67a60a52d9de2c5cb10d35996b
Andrew Jones <joneseyit@protonmail.com>
2018-12-21 11:08:39 -0500

    right before I mess with HTML

--- src/index.js
+++ src/index.js
@@ -27,6 +27,7 @@ function renderImage(json){
   document.querySelector("#likes").innerHTML = json.like_count
   document.querySelector("#comments").innerHTML = commentsHTML
   addLikesHandler()
+  addCommentHandler()
 }
 
 function addLikesHandler(){
@@ -35,11 +36,41 @@ function addLikesHandler(){
 }
 
 function processLike(event){
+  //why is this a post request.  Seems like it should be a patch.
   event.preventDefault()
   let likesId = event.target.parentNode.querySelector("#likes")
   let likesNum = parseInt(likesId.innerHTML)
   likesNum += 1
   likesId.innerHTML = likesNum
+
+  let request = new Request(likeURL)
+  let options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      image_id: 1746,
+      like_count: likesNum
+    })
+  }
+  fetch(request, options)
+}
+
+function addCommentHandler(){
+  //finds comment form
+  let form = document.querySelector("#comment_form")
+  form.addEventListener("submit", processComment)
+}
+
+function processComment(event){
+  //what does this do?  explain it to me again please
+event.preventDefault
+  //refactor search?
+  let comment = event.target.querySelector("#comment_input").value
+  let commentContainer = document.querySelector("#comments")
+  commentContainer.innerHTML += `<li>${comment}</li>`
+
 }
 
 // 1746
