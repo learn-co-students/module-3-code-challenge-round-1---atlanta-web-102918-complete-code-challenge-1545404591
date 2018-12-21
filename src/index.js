5c420bd2f2c6dffe7185cad758973ef26c3c812b
Kaeland Chatman <kaeland1@gmail.com>
2018-12-21 11:04:50 -0500

    Working like button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 1748 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +9,41 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  fetchImage(imageURL)
+  likeHandler(imageURL)
 })
+
+function fetchImage(imageURL) {
+  return fetch(imageURL)
+    .then(res => res.json())
+    // .then(console.log)
+    .then(updateHTML)
+}
+
+function updateHTML(data) {
+  console.log('From inside updateHTML():', data)
+
+  let { name, like_count, comments } = data
+  const imageTitle = document.querySelector('h4#name')
+  const likes = document.querySelector('span#likes')
+  let commentSection = document.querySelector('ul#comments')
+
+  imageTitle.innerText = name
+  likes.innerText = like_count.toString()
+  comments.forEach(({ id, content }) => {
+    commentSection.innerHTML += `
+      <li id="${id}">${content}</li>
+    `
+  })
+}
+
+function likeHandler(url) {
+  let likeButton = document.querySelector('#like_button')
+  likeButton.addEventListener('click', increaseLikes)
+}
+
+function increaseLikes(e) {
+  let likes = document.querySelector('#likes')
+  likes.innerText = (parseInt(likes.innerText) + 1).toString() 
+}
+
