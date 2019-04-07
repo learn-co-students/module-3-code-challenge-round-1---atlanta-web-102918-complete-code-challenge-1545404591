43c7da9b3640e584d15795c35edb110cbb2c52d6
wgeorge1990 <williameverettgeorge@icloud.com>
2019-04-07 12:31:58 -0400

    Refactored and debugged past work.

--- src/index.js
+++ src/index.js
@@ -1,73 +1,88 @@
-document.addEventListener('DOMContentLoaded', initPage())
+document.addEventListener('DOMContentLoaded', init())
 
-  let imageId = 1747
+let imageId = 2357
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
-  const likeURL = `https://randopic.herokuapp.com/likes/`
+const likeURL = `https://randopic.herokuapp.com/likes/`
 
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
+const commentsURL = `https://randopic.herokuapp.com/comments/`
 
-function initPage(){
-  fetchImageObject()
-  likeHandler()
-  addFormHandler()
+function init() {
+  console.log('init')
+  fetchImage()
+  addSubmitHandler()
+
 }
 
-function fetchImageObject(){
-  return fetch(`https://randopic.herokuapp.com/images/1747`)
-  .then(function(resp)  {return resp.json()})
-  .then(function (data) {return renderData(data)})
+function fetchImage() {
+  fetch("https://randopic.herokuapp.com/images/2357")
+    .then(function (response) { return response.json() })
+    .then(function (data) { renderImage(data) })
 }
 
-function renderData(data){
-  document.querySelector('#image').src = data.url
-  document.querySelector('#name').innerText = data.name
-  document.querySelector('#likes').innerText = data.like_count
-  let commentContainer = document.querySelector('#comments')
+function renderImage(data) {
+  let img = document.querySelector('#image')
+  img.dataId = data.id
+  img.dataImg = data
+  img.src = data.url
+  let title = document.querySelector('#name')
+  title.innerText = data.name
+  let likes = document.querySelector('#likes')
+  likes.innerText = data['like_count']
+  data.comments.forEach(renderComment)
+  console.log(data)
+}
 
-  let comments = data.comments
-
-  comments.forEach(function(comment){
-  let contentHtml = `<li>${comment.content}</li>`
-  commentContainer.innerHTML += contentHtml
+function renderComment(comment) {
+  let html = `<li id=${comment.id}>${comment.content || comment}<button data-id=${comment.id} class="button" >Delete</button></li>`
+  let commentsContainer = document.querySelector('#comments')
+  commentsContainer.innerHTML += html
+  let deleteButtons = document.querySelectorAll(`.button`)
+  console.log(deleteButtons)
+  deleteButtons.forEach(function (button) {
+    button.addEventListener('click', deleteTodoItem)
   })
 }
 
-function likeHandler(){
-  var likeButton = document.querySelector('#like_button')
-  likeButton.addEventListener('click', incrementLikes)
+function deleteTodoItem(event) {
+  event.preventDefault();
+  let commentId = event.target.dataset['id']
+  fetch(`https://randopic.herokuapp.com/comments/${commentId}`, {
+    method: 'DELETE',
+    headers: { "Content-Type": "application/json" }
+  })
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+  let comment = document.getElementById(`${commentId}`)
+  comment.remove()
+
 }
 
-function incrementLikes(event){
-  event.preventDefault()
-  let likeCount = parseInt(document.querySelector('#likes').innerText)
-  likeCount += 1
-  document.querySelector('#likes').innerText = likeCount
-
-
-  let request = new Request(likeURL)
-  let options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      image_id: imageId,
-      like_count: likeCount
-    })
-  }
-  fetch(request, options)
-}
-
-function addFormHandler(){
+function addSubmitHandler() {
   let form = document.querySelector('#comment_form')
-  form.addEventListener("submit", processComment)
+  form.addEventListener('submit', proccessCommentForm)
 }
 
-function processComment(event){
-  event.preventDefault()
-  let commentValue = event.target.comment.value
-  let comments = document.querySelector('#comments')
-  comments.innerHTML += `<li>${commentValue}</li>`
+function proccessCommentForm(e) {
+  e.preventDefault();
+  let comment = e.target.comment.value
+  let img = document.querySelector('#image')
+  console.log('inside proccessCommentForm', comment)
+  renderComment(comment)
+  e.target.reset()
+  console.log(img['dataId'])
+  let data = { content: comment, image_id: img['dataId'] }
+  fetch(commentsURL, {
+    method: 'POST',
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+
 }
